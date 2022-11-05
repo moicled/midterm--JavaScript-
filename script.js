8b08ae8258edf8325c87756fe767ac9ea877fee6
moicled <65125125+moicled@users.noreply.github.com>
2022-11-05 16:23:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+var carYear = propmt("what year do what the car?");
+ carYear = carYear.toUpperCase();
+cosole.log("carYeah = " + carYear);
+var year= "Unknown";
+
+var carModel = propmt("what model do you want the car?);
+ carModel = carModel.toUpperCase();
+cosole.log("carModel = " + carModel);
+
 
